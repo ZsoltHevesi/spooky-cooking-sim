981a443cdaa1a0363fb4106a0fc3bddad71b24e0
Georgy Kachelkin <d22124642@mytudublin.ie>
2024-11-05 13:26:45 +0000

    finished configuring collision layers

--- player/interaction_raycast.gd
+++ player/interaction_raycast.gd
@@ -4,6 +4,5 @@ func _physics_process(delta):
 	if is_colliding():
 		if get_collider() is Node:
 			if get_collider().is_in_group("stove"):
-				print("STOVE TOUCHER")
 				if Input.is_action_just_released("interact"):
 					get_collider().use()
