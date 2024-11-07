3595b6df8608a4ae45f5522810645f23c0196085
Georgy Kachelkin <d22124642@mytudublin.ie>
2024-11-07 15:21:15 +0000

    now you can exit the oven

--- _Scenes/stove.gd
+++ _Scenes/stove.gd
@@ -1,22 +1,33 @@
 extends Interactible
 @onready var camera_3d: Camera3D = $Camera3D
-@onready var stove_menu: Control = $stove_menu
 @onready var stove: StaticBody3D = $"."
 
 var top_bun = load("res://_Scenes/burger_top_bun.tscn")
 var top_bun_instance
+var is_in_use : bool = false
 
+func _ready() -> void:
+	set_physics_process(false)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("RMB") and is_in_use:
+		camera_3d.current = false
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		get_node("../player").hud.visible = true
+		set_physics_process(false)
 
 func _on_interacted(body: Variant) -> void:
-	camera_3d.current = true
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	stove_menu.visible = true
-	get_node("../player").hud.visible = false
+	if !is_in_use:
+		camera_3d.current = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		get_node("../player").hud.visible = false
+		is_in_use = true
+		set_physics_process(true)
 
-func _on_button_pressed() -> void:
-	top_bun_instance = top_bun.instantiate()
-	top_bun_instance.position = stove.position
-	top_bun_instance.position.y = 0.6
-	top_bun_instance.transform.basis = stove.transform.basis
-	stove.add_child(top_bun_instance)
+func _physics_process(delta: float) -> void:
+	print("STOVE")
+	#top_bun_instance = top_bun.instantiate()
+	#top_bun_instance.position = stove.position
+	#top_bun_instance.position.y = 0.6
+	#top_bun_instance.transform.basis = stove.transform.basis
+	#stove.add_child(top_bun_instance)
