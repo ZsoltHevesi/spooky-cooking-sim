378d582ae42b4dec656d683f3c8d7f75dd3f5ba6
Georgy Kachelkin <d22124642@mytudublin.ie>
2025-01-04 16:58:11 +0000

    small changes to spawn properties descriptions

--- _Scripts/spawn.gd
+++ _Scripts/spawn.gd
@@ -4,10 +4,11 @@ class_name Spawner
 @onready var spawn: Area3D = $"."
 @onready var spawn_mesh_3d: MeshInstance3D = $MeshInstance3D
 @onready var spawn_collision_3d: CollisionShape3D = $CollisionShape3D
+
 @export var entity : Resource ## Entity that will be instantiated by the spawner.
 @export var spawn_mesh : Mesh ## The [param Mesh] resource for the spawn.
-@export var unlimited : bool ## If [code]true[/code], makes the spawner unlimited. By default spawner works only once (cont. in [param spawned]).
-@export var spawned : bool ## If [code]true[/code], the spawner is marked as used and will not instantiate again until [param spawned] is [code]false[/code].
+@export var unlimited : bool ## If [code]true[/code], makes the spawner unlimited, ignoring [param spawned]. By default spawner works only once (cont. in [param spawned]).
+@export var spawned : bool ## If [code]true[/code], the spawner will not instantiate again until [param spawned] is turned [code]false[/code].
 @export_custom(PROPERTY_HINT_LINK, "") var mesh_scale : Vector3 = Vector3(1.0, 1.0, 1.0) ## Sets the scale value of the spawn [param Mesh].
 var instance
 
